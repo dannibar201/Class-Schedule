c9e427dfbce67e44bb9c33929c7bcff2bbe1488e
20176 <20176@MININT-VTTS8LK.columbushs.com>
2019-09-04 14:24:59 -0400

    Done

---
+++ src/pkgclass/schedule/ClassSchedule.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.schedule;
+
+/**
+ *
+ * @author 20176
+ */
+public class ClassSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Juan Valdez";
+        int graduate
+        graduate = 2010;
+        
+         //Part 1 using variables
+      
+        System.out.println("My name is " + name + " and I'll graduate in " + graduate + ".");
+        
+                
+                
+        //Part 2 your schedule        
+        String math = "Precalculus";
+        String english = "English";
+        String compapp = "Computer Applications";
+        String economic = "Microeconomics";
+        String yearbook = "Yearbook";
+        String compsci = "Computer Science";
+        String socialjus = "Social Justice";
+        String math2 = "Ms. Leonard";
+        String english2 = "Ms. Essig";
+        String compapp2 = "Mr. Moritz";
+        String economic2 = "Mr. Ciocca";
+        String yearbook2 = "Mr. Diaz";
+        String compsci2 = "Mr. Larrubia";
+        String socialjus2 = "Mr.Davis";
+        
+        System.out.println("+==================================================+");
+        System.out.println("| 1 |                     " + math + " | " + math2 + "|");
+        System.out.println("| 2 |                         " + english + " |   " + english2 + "|");
+        System.out.println("| 3 |           " + compapp + " |  " + compapp2 + "|");
+        System.out.println("| 4 |  " + economic + " | " + economic2 + "|");
+        System.out.println("| 5 |  " + yearbook + " | " + yearbook2 + "|");
+        System.out.println("| 6 |  " + compsci + " | " + compsci2 + "|");
+        System.out.println("| 7 |  " + socialjus + " | " + socialjus2 + "|");
+        System.out.println("+==================================================+");
+        
+        
+        
+        
+        
+                
+                
+             
+                
+        
+        
+        
+      
+        
+        
+        
+        
+        
+        
+    }
+    
+}
